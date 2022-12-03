2943df9b2913f083dc5c56482cfe48795909038c
Daniel <danielodom23@gmail.com>
2022-12-03 14:19:20 -0500

    first commit

---
+++ openai.js
@@ -0,0 +1,58 @@
+//Library Imports
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+// Global variables
+const { TOKEN, SERVER_URL, BUILD, PORT } = process.env;
+
+// Function Imports
+const { generate } = require("./utils/generate");
+const { sendMessage } = require("./utils/sendResponse");
+//Express
+const app = express();
+app.use(bodyParser.json());
+
+//Webhook
+let serverUrl = SERVER_URL;
+if (BUILD == "Test") {
+  serverUrl = "https://d657-2601-589-4d80-16d0-c17a-1adf-e13e-5716.ngrok.io";
+}
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/app2/${TOKEN}`;
+const WEBHOOK_URL = serverUrl + URI;
+
+app.post(URI, async (req, res) => {
+  try {
+    if (req.body.message.chat) {
+      const chatId = req.body.message.chat.id;
+      const command = req.body.message.text;
+      const messageId = req.body.message.message_id;
+      if (command.split(" ")[0] == "/ask") {
+        const question = command.slice(5);
+        if (question == "test" || question == "test?") {
+          sendMessage(TELEGRAM_API, chatId, "*What exactly are you testing?*", messageId);
+        } else if (question) {
+          generate(question).then((response) => sendMessage(TELEGRAM_API, chatId, response, messageId));
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  return res.send();
+});
+
+app.listen(PORT || 5000, () => {
+  console.log("🚀 app running on port", PORT || 5000);
+
+  // Delete and unsubscribe to webhook events
+  axios.get(`${TELEGRAM_API}/deleteWebhook?drop_pending_updates=true`).then((res) => {
+    console.log(res.data);
+    // Subscribe to webhook events
+    axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`).then((res) => {
+      console.log(res.data);
+    });
+  });
+});
---
+++ utils/generate.js
@@ -0,0 +1,24 @@
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+const { OPENAI_KEY } = process.env;
+console.log(OPENAI_KEY);
+const configuration = new Configuration({
+  apiKey: OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const generate = async (input) => {
+  console.log("ping");
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: input,
+    temperature: 1,
+    max_tokens: 2056,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return response.data.choices[0].text;
+};
+
+module.exports = { generate };
