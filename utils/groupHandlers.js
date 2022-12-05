fc0e6ad621163fb6fd08cae7631405a8130959ec
Daniel <danielodom23@gmail.com>
2022-12-05 01:40:50 -0500

    update

--- utils/groupHandlers.js
+++ utils/groupHandlers.js
@@ -2,45 +2,45 @@
 const fs = require("fs");
 
 const addNewGroup = (id, title, groupType) => {
-  let data = fs.readFileSync("./groupData.json", "utf-8");
+  let data = fs.readFileSync("./data/groupData.json", "utf-8");
   data = JSON.parse(data);
   const found = data.groups.findIndex(({ chatId }) => chatId === id);
   if (found === -1) {
     data.groups.push({ chatId: id, name: title, type: groupType, request: 1 });
-    fs.writeFileSync("./groupData.json", JSON.stringify(data));
+    fs.writeFileSync("./data/groupData.json", JSON.stringify(data));
     return true;
   }
   updateGroup(found, title);
 };
 const addNewPrivate = (id, username) => {
-  let data = fs.readFileSync("./groupData.json", "utf-8");
+  let data = fs.readFileSync("./data/groupData.json", "utf-8");
   data = JSON.parse(data);
   const found = data.private.findIndex(({ chatId }) => chatId === id);
   if (found === -1) {
     data.private.push({ chatId: id, name: username, request: 1 });
-    fs.writeFileSync("./groupData.json", JSON.stringify(data));
+    fs.writeFileSync("./data/groupData.json", JSON.stringify(data));
     return true;
   }
   updatePrivate(found, username);
 };
 
 const updateGroup = (found, title) => {
-  let data = fs.readFileSync("./groupData.json", "utf-8");
+  let data = fs.readFileSync("./data/groupData.json", "utf-8");
   data = JSON.parse(data);
   data.groups[found].name = title;
   data.groups[found].request += 1;
-  fs.writeFileSync("./groupData.json", JSON.stringify(data));
+  fs.writeFileSync("./data/groupData.json", JSON.stringify(data));
 };
 const updatePrivate = (found, username) => {
-  let data = fs.readFileSync("./groupData.json", "utf-8");
+  let data = fs.readFileSync("./data/groupData.json", "utf-8");
   data = JSON.parse(data);
   data.private[found].name = username;
   data.private[found].request += 1;
-  fs.writeFileSync("./groupData.json", JSON.stringify(data));
+  fs.writeFileSync("./data/groupData.json", JSON.stringify(data));
 };
 
 const getMetrics = () => {
-  let data = fs.readFileSync("./groupData.json", "utf-8");
+  let data = fs.readFileSync("./data/groupData.json", "utf-8");
   data = JSON.parse(data);
   let text = "";
   let requests = 0;
