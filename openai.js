babdd9c246d08d841fb9348523d6233131d8c926
Daniel <danielodom23@gmail.com>
2022-12-07 09:34:08 -0500

    exploit fidner

--- openai.js
+++ openai.js
@@ -73,7 +73,7 @@ app.post(URI, async (req, res) => {
             if (question) {
               generateImage(question).then((response) => {
                 if (response[0] != false) {
-                  console.log(response[0], id);
+                  console.log(response[0], chat.id, chat.username);
                   if (response[1] === "image") {
                     sendPhoto(TELEGRAM_API, chatId, response[0], `${question}\n\n${footerText}`, messageId, false);
                   } else {
