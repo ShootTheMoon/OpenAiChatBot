f5abaaac94a51f78b836a2fa598ed41019d77491
Daniel <danielodom23@gmail.com>
2022-12-10 15:26:30 -0600

    broadcast

--- utils/broadcastMessage.js
+++ utils/broadcastMessage.js
@@ -20,25 +20,21 @@ const broadcast = (ctx, msg = false) => {
       data = JSON.parse(data);
       const groups = data.groups;
       for (let i = 0; i < groups.length; i++) {
-        setTimeout(() => {
-          ctx
-            .sendMessage(
-              `*Twitter Bot!*\n\nOpenAI ERC's twitter bot is now live! You can use it by mentioning the account below followed by a statement or question!\n\nTwitter Bot: [@OpenAIChat_BOT](twitter.com/OpenAIChat_BOT)\n\n_5000$ for whoever gets a reply from Elon or Vitalik using the twitter bot (info). Also, 4000$ Big Buy Competition ends in 18 hours._\n\n${footerAdd}.`,
-              { chat_id: groups[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }
-            )
-            .catch((err) => console.log(err));
-        }, 200);
+        ctx
+          .sendMessage(
+            `*Twitter Bot!*\n\nOpenAI ERC's twitter bot is now live! You can use it by mentioning the account below followed by a statement or question!\n\nTwitter Bot: [@OpenAIChat_BOT](twitter.com/OpenAIChat_BOT)\n\n_7500$ for whoever gets a reply from Elon or Vitalik using the twitter bot (info). Also, 4000$ Big Buy Competition ends in 18 hours._\n\n${footerAdd}.`,
+            { chat_id: groups[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }
+          )
+          .catch((err) => console.log(err));
       }
       const private = data.private;
       for (let i = 0; i < private.length; i++) {
-        setTimeout(() => {
-          ctx
-            .sendMessage(
-              `*Twitter Bot!*\n\nOpenAI ERC's twitter bot is now live! You can use it by mentioning the account below followed by a statement or question!\n\nTwitter Bot: [@OpenAIChat_BOT](twitter.com/OpenAIChat_BOT)\n\n_5000$ for whoever gets a reply from Elon or Vitalik using the twitter bot (info). Also, 4000$ Big Buy Competition ends in 18 hours._\n\n${footerAdd}.`,
-              { chat_id: private[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }
-            )
-            .catch((err) => console.log(err));
-        }, 200);
+        ctx
+          .sendMessage(
+            `*Twitter Bot!*\n\nOpenAI ERC's twitter bot is now live! You can use it by mentioning the account below followed by a statement or question!\n\nTwitter Bot: [@OpenAIChat_BOT](twitter.com/OpenAIChat_BOT)\n\n_7500$ for whoever gets a reply from Elon or Vitalik using the twitter bot (info). Also, 4000$ Big Buy Competition ends in 18 hours._\n\n${footerAdd}.`,
+            { chat_id: private[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }
+          )
+          .catch((err) => console.log(err));
       }
       console.log("Broadcast done");
     }
