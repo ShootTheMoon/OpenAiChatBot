46be4d0c9516f444ccece37db0d1f9f664740320
Daniel <danielodom23@gmail.com>
2022-12-15 00:24:36 -0500

    tts and broadcast

--- utils/broadcastMessage.js
+++ utils/broadcastMessage.js
@@ -5,21 +5,20 @@ const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 const broadcast = async (ctx, msg = false) => {
   try {
     if (msg) {
-      console.log("ping");
+      const messageId = ctx.message.message_id;
       let data = fs.readFileSync("./data/groupData.json");
       data = JSON.parse(data);
       const groups = data.groups;
       for (let i = 0; i < groups.length; i++) {
-        console.log("ping group");
-        ctx.sendMessage(`${msg}`, { chat_id: groups[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }).catch((err) => console.log(err));
+        ctx.sendMessage(`${msg}`, { chat_id: groups[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }).catch((err) => {});
         await timer(300);
       }
       const private = data.private;
       for (let i = 0; i < private.length; i++) {
-        console.log("ping private");
-        ctx.sendMessage(`${msg}`, { chat_id: private[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }).catch((err) => console.log(err));
+        ctx.sendMessage(`${msg}`, { chat_id: private[i].chatId, disable_web_page_preview: true, parse_mode: "Markdown" }).catch((err) => {});
         await timer(300);
       }
+      ctx.reply(`Broadcasting finished`, { parse_mode: "Markdown", disable_web_page_preview: true, reply_to_message_id: messageId }).catch((err) => {});
     }
   } catch (err) {}
 };
